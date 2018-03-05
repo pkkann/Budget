3a3d68975b02833e0675feab1ff8ff96abb18afb
Patrick Kann <pkkann@gmail.com>
2018-03-05 15:50:38 +0100

    w

--- budget.sql
+++ budget.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Vært: localhost
--- Genereringstid: 02. 03 2018 kl. 15:29:33
+-- Genereringstid: 05. 03 2018 kl. 15:50:20
 -- Serverversion: 10.1.30-MariaDB
 -- PHP-version: 5.6.33
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `budget` (
   `id` int(11) UNSIGNED NOT NULL,
-  `year` int(4) UNSIGNED NOT NULL,
+  `name` varchar(45) COLLATE utf8_danish_ci NOT NULL,
   `inserttimestamp` int(10) NOT NULL,
   `updatetimestamp` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
@@ -39,9 +39,9 @@ CREATE TABLE `budget` (
 -- Data dump for tabellen `budget`
 --
 
-INSERT INTO `budget` (`id`, `year`, `inserttimestamp`, `updatetimestamp`) VALUES
-(1, 2018, 1519999230, 1519999230),
-(2, 2017, 1520000614, 1520000614);
+INSERT INTO `budget` (`id`, `name`, `inserttimestamp`, `updatetimestamp`) VALUES
+(3, 'Patrick', 1520249716, 1520249716),
+(4, 'Thea', 1520249756, 1520249756);
 
 -- --------------------------------------------------------
 
@@ -51,11 +51,10 @@ INSERT INTO `budget` (`id`, `year`, `inserttimestamp`, `updatetimestamp`) VALUES
 
 CREATE TABLE `post` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(75) COLLATE utf8_danish_ci NOT NULL,
-  `amount` decimal(7,2) NOT NULL,
-  `type` int(1) UNSIGNED NOT NULL,
-  `budget_id` int(11) UNSIGNED NOT NULL,
-  `inserttimestamp` int(10) NOT NULL,
+  `name` varchar(45) COLLATE utf8_danish_ci NOT NULL,
+  `type` enum('expense','income') COLLATE utf8_danish_ci NOT NULL,
+  `budget_id` int(10) UNSIGNED NOT NULL,
+  `createtimestamp` int(10) NOT NULL,
   `updatetimestamp` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
@@ -63,8 +62,10 @@ CREATE TABLE `post` (
 -- Data dump for tabellen `post`
 --
 
-INSERT INTO `post` (`id`, `name`, `amount`, `type`, `budget_id`, `inserttimestamp`, `updatetimestamp`) VALUES
-(1, 'Husleje', '5600.00', 0, 1, 1520000067, 1520000067);
+INSERT INTO `post` (`id`, `name`, `type`, `budget_id`, `createtimestamp`, `updatetimestamp`) VALUES
+(1, 'Fælles budget', 'expense', 3, 1520256768, 1520256768),
+(2, 'Fælles rådighed', 'expense', 3, 1520256795, 1520256795),
+(3, 'Løn', 'income', 3, 1520256842, 1520256842);
 
 -- --------------------------------------------------------
 
@@ -96,8 +97,7 @@ INSERT INTO `user` (`id`, `name`, `username`, `password`, `updatetimestamp`, `in
 -- Indeks for tabel `budget`
 --
 ALTER TABLE `budget`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `year` (`year`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks for tabel `post`
@@ -120,13 +120,13 @@ ALTER TABLE `user`
 -- Tilføj AUTO_INCREMENT i tabel `budget`
 --
 ALTER TABLE `budget`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `user`
